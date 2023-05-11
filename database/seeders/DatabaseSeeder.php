176e3f1732367136470073ca2ecaceea242e5119
romanmanko-KH12 <roman.mnako@oa.edu.ua>
2023-05-11 19:58:31 +0300

    lab2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    { $this->call(UsersTableSeeder::class);
+        $this->call(BlogCategoriesTableSeeder::class);
+        \App\Models\BlogPost::factory(100)->create();
         // \App\Models\User::factory(10)->create();
     }
 }
